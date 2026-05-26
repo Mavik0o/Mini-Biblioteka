ec3966152a5b631b647438068e025ee04407af24
Łukasz Dziubiela <s33646@pjwstk.edu.pl>
2026-05-26 13:01:01 +0200

    troche library zrobione, pozniej zrobie reszte

--- app/library.py
+++ app/library.py
@@ -1,5 +1,40 @@
+from datetime import date
+
+from app.models import Book, User, Loan
+from app.exceptions import (
+    BookNotFoundError,
+    UserNotFoundError,
+    BookNotAvailableError,
+    LoanNotFoundError
+)
+
+
 class Library:
     def __init__(self):
         self.books = []
         self.users = []
         self.loans = []
+
+    def add_book(self, title, author, year, category):
+        pass
+
+    def add_user(self, first_name, last_name, email):
+        pass
+
+    def find_book_by_id(self, book_id):
+        pass
+
+    def find_user_by_id(self, user_id):
+        pass
+
+    def borrow_book(self, book_id, user_id):
+        pass
+
+    def return_book(self, book_id):
+        pass
+
+    def get_available_books(self):
+        pass
+
+    def get_borrowed_books(self):
+        pass
---
+++ app/storage.py
@@ -0,0 +1,4 @@
+import json
+from app.library import Library
+from app.models import Book, User, Loan
+
